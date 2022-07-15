c714087584263b2cd2636ce8bb06eebbd4e5faec
Collins <mussocollins@gmail.com>
2022-07-15 06:12:38 +0300

    Make changes to fix Eslint errors

--- src/completedtasks.js
+++ src/completedtasks.js
@@ -1,15 +1,15 @@
 const storetask = (todoList) => {
-    localStorage.setItem('todo-list-group', JSON.stringify(todoList));
-  };
-  
-  const gettasks = () => JSON.parse(localStorage.getItem('todo-list-group'));
-  // Make checkbox true for completed taskss
-  export default (e) => {
-    const list = gettasks();
-    if (e.target.checked === true) {
-      list[e.target.id].completed = true;
-    } else {
-      list[e.target.id].completed = false;
-    }
-    storetask(list);
-  };
+  localStorage.setItem("todo-list-group", JSON.stringify(todoList));
+};
+
+const gettasks = () => JSON.parse(localStorage.getItem("todo-list-group"));
+// Make checkbox true for completed taskss
+export default (e) => {
+  const list = gettasks();
+  if (e.target.checked === true) {
+    list[e.target.id].completed = true;
+  } else {
+    list[e.target.id].completed = false;
+  }
+  storetask(list);
+};
