643b0377dccb0e2f144e826cc199fdcca5205d17
Collins <mussocollins@gmail.com>
2022-07-15 16:46:44 +0300

    Make changes

--- src/localstorage.js
+++ src/localstorage.js
@@ -35,12 +35,7 @@ export default (() => {
     list.splice(index, 1);
     storetask(sortindex(list));
   };
-  // *****Clear Completed tasks
-  const clearCompleted = () => {
-    const list = gettasks();
-    const newList = list.filter((e) => e.completed === false);
-    storetask(sortindex(newList));
-  };
+  
   // ****Return functions
   return {
     sortindex,
@@ -49,6 +44,5 @@ export default (() => {
     add,
     updateTask,
     remove,
-    clearCompleted,
   };
 })();
